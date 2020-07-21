5bf924b559794a7675d5a005fef343787d061b18
Courage Oghogho <oghogho.courage@hotmail.com>
2020-07-21 06:52:59 +0530

    All files upload

---
+++ vehicleTransFleets/src/main/java/me/courageoghogho/VehicleFleetManagement/VehicleTransFleetsApplication.java
@@ -0,0 +1,13 @@
+package me.courageoghogho.VehicleFleetManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleTransFleetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleTransFleetsApplication.class, args);
+	}
+
+}
---
+++ vehicleTransFleets/src/test/java/me/courageoghogho/VehicleFleetManagement/VehicleTransFleetsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.courageoghogho.VehicleFleetManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleTransFleetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
